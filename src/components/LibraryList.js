0639d273dccc4bb33e48079d37fa6c50099b9642
Ajay David <antonio.g.david@gmail.com>
2017-05-25 12:17:38 -0400

    get initial state and pass as prop to LibraryList

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+// connect helper gives component access to redux state in provider store
+import { connect } from 'react-redux';
 
 class LibraryList extends Component {
   render() {
+    console.log(this.props);
     return;
   }
 }
 
-export default LibraryList;
+const mapStateToProps = state => {
+  // any object returned here is a prop for the component
+  return { libraries: state.libraries };
+};
+
+//call connect function, which immediately returns another function
+// include the LibraryList in the immediate call to other function
+export default connect(mapStateToProps)(LibraryList);
