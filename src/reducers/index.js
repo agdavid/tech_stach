d46f857236fae36c3b3894a64dfb250cff87caf5
Ajay David <antonio.g.david@gmail.com>
2017-05-25 14:19:33 -0400

    import SelectionReducer into reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './SelectionReducer';
 
+// reducer keys will be the keys in the app state (e.g., this.state.libraries)
 export default combineReducers({
-  libraries: LibraryReducer
+  libraries: LibraryReducer,
+  selectedLibraryId: SelectionReducer
 });
