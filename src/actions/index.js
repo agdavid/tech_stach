8d5f700a11acd01fcfbaefa8e3b4050c2a9c89fd
Ajay David <antonio.g.david@gmail.com>
2017-05-25 14:29:55 -0400

    create shell for selectLibrary action creator

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+// the action creator - a JS function that returns an action
+export const selectLibrary = () => {
+  // the action - dispatched to reducers
+  return {
+    type: 'select_library'
+  };
+};
