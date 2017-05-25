424b706fb175f50b7846227e869b550ba8d391d3
Ajay David <antonio.g.david@gmail.com>
2017-05-25 15:18:03 -0400

    refactor SelectionReducer to use standard switch statement

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,5 +1,13 @@
-export default (state, action) => {
-  // reducer must have initial state; cannot be undefined
-  console.log(action);
-  return null;
+// if state is undefined, return null to avoid error  
+export default (state = null, action) => {
+  // reducer boilerplate for responding to actions
+  switch (action.type) {
+    case 'select_library':
+      return action.payload;
+    default:
+      // action comes through that reducer should not respond to
+      // just return the previous state
+      return state;
+  }
+
 };
